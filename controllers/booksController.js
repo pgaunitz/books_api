7cc4c6a7099cd012a4e5bf966e1bfeb428c7f3d4
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-08 16:52:56 +0200

    formatting and removes comments

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,18 +3,13 @@ const models = require("../database/models");
 const booksController = {
   async index(req, res, next) {
     const booksIndex = await models.Book.findAll();
-  res.json({ books: booksIndex })
-},
-async show(req, res, next) {
-  const { id } =req.params
-  const book = await models.Book.findByPk(id)
-  res.json({ book: book })
-}
-}
-
-// const index = async (req, res, next) => {
-//   const booksIndex = await models.Book.findAll();
-//   res.json({ books: books });
-// };
+    res.json({ books: booksIndex });
+  },
+  async show(req, res, next) {
+    const { id } = req.params;
+    const book = await models.Book.findByPk(id);
+    res.json({ book: book });
+  },
+};
 
 module.exports = booksController;
--- specs/booksEndpoint.spec.js
+++ specs/booksEndpoint.spec.js
@@ -1,51 +1,43 @@
-const app = require('../app')
-const supertest = require('supertest')
-const expect = require('chai').expect
-const jsonResponse =require('./jsonResponse')
+const app = require("../app");
+const supertest = require("supertest");
+const expect = require("chai").expect;
+const jsonResponse = require("./jsonResponse");
 
-let server, request, response
+let server, request, response;
 
-before((done)=> {
-  server = app.listen(done)
-  request = supertest.agent(server)
+before((done) => {
+  server = app.listen(done);
+  request = supertest.agent(server);
+});
 
-})
+after((done) => {
+  server.close(done);
+});
 
-after((done)=> {
-  server.close(done)
-})
-
-describe('GET /api/v1/books', () => {
+describe("GET /api/v1/books", () => {
   before(async () => {
-    response = await request.get('/api/v1/books')
-  })
-  it('responds with satus 200', () => {
-    expect(response.status).to.equal(200)
-  })
-
-  // it('responds with list of books', () => {
-  //   expect(Array.isArray(response.body.books))
-  //   .to.equal(true)
-  // })
-
-  it('responds with list of books', () => {
-    console.table(response.body.books)
-    expect(response.body.books)
-    .to.be.an('array')
-  })
-
-  it('retruns title', () => {
-    expect(response.body.books[0].title).to.equal('Learn NodeJS')
-    
+    response = await request.get("/api/v1/books");
+  });
+  it("responds with satus 200", () => {
+    expect(response.status).to.equal(200);
   });
 
-  describe('GET /api/v1/books/:id', () => {
-    before(async () => {
-      response = await request.get('/api/v1/books/2')
-    })
+  it("responds with list of books", () => {
+    console.table(response.body.books);
+    expect(response.body.books).to.be.an("array");
+  });
 
-    it('response with single book', () => {
-    expect(response.body.book.id).to.equal(2)      
+  it("retruns title", () => {
+    expect(response.body.books[0].title).to.equal("Learn NodeJS");
+  });
+
+  describe("GET /api/v1/books/:id", () => {
+    before(async () => {
+      response = await request.get("/api/v1/books/2");
     });
-  })
-})
+
+    it("response with single book", () => {
+      expect(response.body.book.id).to.equal(2);
+    });
+  });
+});
